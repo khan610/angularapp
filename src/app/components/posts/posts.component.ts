a323b32ef6a9108cad6d8a0b6469333b336478d7
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-05-01 21:14:56 +0200

    Delete Http Request part 2

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -51,7 +51,13 @@ export class PostsComponent implements OnInit {
 
   removePost(post: Post) {
     if(confirm('Are you sure?')) {
-      this.postService.removePost(post.i)
+      this.postService.removePost(post.id).subscribe(() => {
+        this.posts.forEach((cur, index) => {
+          if(post.id === cur.id) {
+            this.posts.splice(index, 1);
+          }
+        });
+      });
     }
   }
 
